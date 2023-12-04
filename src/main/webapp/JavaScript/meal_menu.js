b89bcb6e15626820966c52106b3444828596a4ea
tanuma <h.tanuma.sys22@morijyobi.ac.jp>
2023-12-04 12:04:45 +0900

    食事メニューのcss

--- src/main/webapp/JavaScript/meal_menu.js
+++ src/main/webapp/JavaScript/meal_menu.js
@@ -15,3 +15,19 @@ function searchByCategory(category_Id) {
     function redirectToDetail(foodId) {
         window.location.href = "MealMenuDetailServlet?id=" + foodId;
     }
+    
+    
+document.addEventListener('DOMContentLoaded', function () {
+  // カードの数を取得
+  var cardCount = document.querySelectorAll('.card-body').length;
+
+  // カードが1枚の場合、左寄せのクラスを追加
+  if (cardCount === 1) {
+    document.querySelector('.card-container').classList.add('left-align');
+  }
+
+  // カードが2枚以上の場合、中央寄せのクラスを追加
+  if (cardCount > 1) {
+    document.querySelector('.card-container').classList.add('center-cards');
+  }
+});
