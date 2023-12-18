a23639cbd61ce37defc9fecaa61711d16c137cae
tanifuji <h.tanifuji.sys22@morijyobi.ac.jp>
2023-12-18 13:01:27 +0900

    記録のTOP画面のグラフ（形だけ完成）

--- src/main/java/servlet/UserLogServlet.java
+++ src/main/java/servlet/UserLogServlet.java
@@ -1,30 +1,45 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class UserLogServlet
+ */
 @WebServlet("/UserLogServlet")
 public class UserLogServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json");
-        try (PrintWriter out = response.getWriter()) {
-            // ここで必要なデータを取得し、JSON形式でクライアントに返す（模擬的な例）
-            String jsonData = "{ \"data\": [5, 8, 3, 10, 6, 12, 4, 9, 7, 11, 8, 5, 10, 6] }";
-            out.println(jsonData);
-        }
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserLogServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String view = "WEB-INF/view/user-log.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
