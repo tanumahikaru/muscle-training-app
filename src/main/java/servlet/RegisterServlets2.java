2926525e9cfc7a378799e4b32ef407cf5d6e8c92
tanifuji <h.tanifuji.sys22@morijyobi.ac.jp>
2023-12-19 10:49:09 +0900

    治れ！！

--- src/main/java/servlet/RegisterServlets2.java
+++ src/main/java/servlet/RegisterServlets2.java
@@ -15,14 +15,14 @@ import dto.UserDTO;
 /**
  * Servlet implementation class TopServlet
  */
-@WebServlet("/RegisterServlet2")
-public class RegisterServlet2 extends HttpServlet {
+@WebServlet("/RegisterServlets2")
+public class RegisterServlets2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RegisterServlet2() {
+    public RegisterServlets2() {
         super();
         // TODO Auto-generated constructor stub
     }
