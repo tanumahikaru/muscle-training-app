b673e23b3ec4cc301c9f8bb3e06c6aad19a2c5bb
hatakeyama <m.hatakeyama.sys22@morijyobi.ac.jp>
2023-12-22 13:22:12 +0900

    新規登録後の遷移先をindex.jspに変更

--- src/main/java/servlet/RegisterExecuteServlet.java
+++ src/main/java/servlet/RegisterExecuteServlet.java
@@ -55,7 +55,7 @@ public class RegisterExecuteServlet extends HttpServlet {
 	            // 登録に成功したので、sessionのデータを削除
 	            session.removeAttribute("input_data");
 	            session.removeAttribute("weight_data");
-	            path = "WEB-INF/view/home.jsp";
+	            path = "index.jsp";
 	        } else {
 	            // 失敗した場合はパラメータ付きで登録画面に戻す
 	            path = "WEB-INF/view/register-user.jsp?error=1";
