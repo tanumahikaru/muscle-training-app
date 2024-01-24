920d8a5d9c780cd59ce75560d2f25bd4fba15ad9
koha0516 <113155734+koha0516@users.noreply.github.com>
2024-01-24 12:33:48 +0900

    Validate.java を更新

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -7,7 +7,7 @@ public class Validation {
 		this.errorMessage = errorMessage;
 	}
 
-	// nullチェックを行う
+	// nullまたはemptyか判定する
 	private Validation isNullOrEmpty(String str) {
         if (str == null) {
 												result = true;
@@ -22,17 +22,34 @@ public class Validation {
         return new Validation(false, "値は正常です");
 	}
 
-	// 文字列の長さが指定範囲内かどうかをチェックする
-	public boolean stringLength(String str, int minLength, int maxLength) {
-		if(str.length() < minLength && maxLength < str.length()) {
-			// パスワードの長さが指定範囲外だった場合falseを返す
-			return false;
+	// 文字列の長さが指定範囲内か判定する
+	public boolean isInvalidLengthOfString(String str, int minLength, int maxLength) {
+		if(str.length() < minLength) {
+			result = true;
+			errorMessage = minLength + "文字以上で入力してください。";
+			return new Validation(result, errorMessage);
 		}
+		if(maxLength < str.length()) {
+			result = true;
+			errorMessage = minLength + "文字以内で入力してください。";
+			return new Validation(result, errorMessage);
+		}
+
+		return new Validation(false, "");
 	}
 
 
-	public boolean passwordIsValid(String str, int minLength, int maxLength, ) {
+	public Validation passwordIsValid(String password, int minLength, int maxLength, ) {
+		Validation result；
+		if((result = isNullOrEmpty(password))) {
+			return result;
+		}
+		if((result = isInvalidLengthOfString(password))) {
+			return result;
+		}
+		// 他にチェック項目があったら以降に追加する
 
+		return result = new Validation(true);
 	}
 
 // 一旦終わり
