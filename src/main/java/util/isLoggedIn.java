45ab9c0643b33845a0f21bf2af0b0b6699351c65
koha0516 <113155734+koha0516@users.noreply.github.com>
2024-01-24 10:42:01 +0900

    isLoggedIn.java を更新

--- src/main/java/util/isLoggedIn.java
+++ src/main/java/util/isLoggedIn.java
@@ -1,4 +1,9 @@
 import java.util.Objects;
 import dto.UserDTO;
 
-
+		// ログイン済みかどうかの判定
+		if(Objects.isNull(user)) {
+			String view = "/";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			dispatcher.forward(request, response);
+		}
